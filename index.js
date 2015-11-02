1f08522cac5a5697a19166c367a44fcfe21827e1
Dolphin Wood <dolphin.w.e@gmail.com>
2015-11-02 11:44:34 +0800

    update: update with requestIdle@0.2.1

--- index.js
+++ index.js
@@ -1,37 +1,60 @@
 angular.module('ng.requestIdle', [])
-    .factory('TimeManager', ['$timeout', '$q', function($timeout, $q) {
-        function Idle(tm) {
+    .factory('Idle', ['$timeout', '$q', function($timeout, $q){
+        return function Idle(duration) {
             var deferred = $q.defer();
+            var end = duration + Date.now();
+
+            var timer = $timeout(function() {
+                deferred.resolve();
+            }, duration);
+
+            Object.defineProperty(this, 'remain', {
+                get: function() {
+                    return end - Date.now();
+                }
+            });
 
             this.promise = deferred.promise;
 
-            this.end = function(v) {
-                deferred.resolve(v);
-                tm.free();
+            this.end = function() {
+                deferred.resolve();
+                $timeout.cancel(timer);
+            };
+
+            this.add = function(ms) {
+                ms = parseInt(ms);
+
+                if (isNaN(ms)) throw new TypeError('expect ms to be a number, but got ' + typeof ms);
+
+                $timeout.cancel(timer);
+
+                end += ms;
+
+                timer = $timeout(function() {
+                    deferred.resolve();
+                }, end - Date.now());
             };
         };
+    }])
+    .factory('TimeManager', ['Idle', function(Idle) {
+        function TimeManager() {};
 
-        function TimeManager(duration) {
+        TimeManager.prototype.talloc = function(duration) {
             duration = parseInt(duration);
 
-            this.duration = isNaN(duration) ? -1 >>> 1 : duration;
-        };
+            duration = isNaN(duration) ? -1 >>> 1 : duration;
 
-        TimeManager.prototype.talloc = function() {
-            var idle = new Idle(this);
-
-            this.timer = $timeout(idle.end.bind(idle), this.duration);
+            var idle = this.idle = new Idle(duration);
 
             return idle;
         };
 
         TimeManager.prototype.free = function() {
-            $timeout.cancel(this.timer);
+            this.idle.end();
         };
 
         TimeManager.prototype.errorHandler = function(err) {
             this.free();
-
             console.error(err);
         };
 
@@ -50,19 +73,15 @@ angular.module('ng.requestIdle', [])
                 task = arguments[1];
             }
 
-            var tm = new TimeManager(duration);
+            var tm = new TimeManager();
 
-            var currentTask = lastTask.then(function() {
-                    var idle = tm.talloc();
+            lastTask = lastTask.then(function() {
+                    var idle = tm.talloc(duration);
 
                     if (typeof task === 'function') task(idle);
 
                     return idle.promise;
                 })
                 .catch(tm.errorHandler.bind(tm));
-
-            lastTask = currentTask;
-
-            return currentTask;
         };
     }]);
